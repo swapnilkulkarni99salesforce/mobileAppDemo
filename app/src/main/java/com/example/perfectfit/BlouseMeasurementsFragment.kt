97af39284230df24da42376d8691d7d5bece779e
Swapnil Kulkarni <swapnilkulkarni99salesforce@gmail.com>
2025-10-16 22:47:47 +0530

    Fix: Correct import statements syntax error
    - Changed 'android:view.ViewGroup' to 'android.view.ViewGroup'
    - Fixed typo: colon (:) should be dot (.) in import statements
    - Fixes 'Expecting a top level declaration' compilation errors
    - Fixes 'imports are only allowed in the beginning of file' errors

--- app/src/main/java/com/example/perfectfit/BlouseMeasurementsFragment.kt
+++ app/src/main/java/com/example/perfectfit/BlouseMeasurementsFragment.kt
@@ -3,7 +3,7 @@ package com.example.perfectfit
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android:view.ViewGroup
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class BlouseMeasurementsFragment : Fragment() {
